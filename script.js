e16e97bbff753bb45a5284d44a36b857a1f261ba
ana204 <anagomes.lucia@hotmail.com>
2021-01-06 17:38:06 -0300

    form rules

--- script.js
+++ script.js
@@ -46,7 +46,18 @@ let Validator = {
                     break;
 
                     case 'min': 
+                    if(input.value.length < Details[1]){
+                        return 'At least '+Details[1]+' characters ';
+                    }
+                    break;
 
+                    case 'email':
+                        if(input.value != ''){
+                            let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                            if(!regex.test(input.value.toLowerCase())){
+                                return 'Invalid email'
+                            }
+                        }
                     break;
                  }
              }
